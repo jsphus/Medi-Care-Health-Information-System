8c3e180c8067163c881c30f190eebf577bbec63b
franz <condesfranzdominic@gmail.com>
2025-11-16 19:48:40 +0800

    Fix case sensitivity in database configuration file path

--- classes/Auth.php
+++ classes/Auth.php
@@ -6,7 +6,7 @@ class Auth {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
-        require_once __DIR__ . '/../config/database.php';
+        require_once __DIR__ . '/../config/Database.php';
         $this->db = Database::getInstance();
     }
     
